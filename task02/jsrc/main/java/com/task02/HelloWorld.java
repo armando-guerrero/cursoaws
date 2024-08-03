0029264df7b004d1d4cc3d69e3dde8b69d144edf
armando-guerrero <armando.guerrero@externos.digitalfemsa.com>
2024-08-02 19:21:41 -0600

    Task02-1

---
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -0,0 +1,116 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+
+@LambdaHandler(
+    lambdaName = "hello_world",
+	roleName = "hello_world-role",
+	isPublishVersion = false,
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED,
+	layers = {"sdk-layer"},
+	runtime = DeploymentRuntime.JAVA17,
+	architecture = Architecture.ARM64
+)
+
+@LambdaLayer(
+		layerName = "sdk-layer",
+		libraries = {"jsrc/main/java/com/task02/lib/commons-lang3-3.14.0.jar", "jsrc/main/java/com/task02/lib/gson-2.10.1.jar"},
+		runtime = DeploymentRuntime.JAVA17,
+		architectures = {Architecture.ARM64},
+		artifactExtension = ArtifactExtension.ZIP
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+
+	private static final int SC_OK = 200;
+	private static final int SC_NOT_FOUND = 404;
+	private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	private final Map<String, String> responseHeaders = Map.of("Content-Type", "application/json");
+	private final Map<HelloWorld.RouteKey, Function<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse>> routeHandlers = Map.of(
+			new HelloWorld.RouteKey("GET", "/"), this::handleGetRoot,
+			new HelloWorld.RouteKey("GET", "/hello"), this::handleGetHello
+	);
+
+	@Override
+	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
+		HelloWorld.RouteKey routeKey = new HelloWorld.RouteKey(getMethod(requestEvent), getPath(requestEvent));
+		return routeHandlers.getOrDefault(routeKey, this::notFoundResponse).apply(requestEvent);
+	}
+
+	private APIGatewayV2HTTPResponse handleGetRoot(APIGatewayV2HTTPEvent requestEvent) {
+		return buildResponse(SC_OK, HelloWorld.Body.ok("Use the path /hello to get greetings message"));
+	}
+
+	private APIGatewayV2HTTPResponse handleGetHello(APIGatewayV2HTTPEvent requestEvent) {
+		return buildResponse(SC_OK, HelloWorld.Body.ok("Hello%s".formatted(
+				Optional.ofNullable(requestEvent.getQueryStringParameters())
+						.map(this::getUserName)
+						.map(", %s"::formatted)
+						.orElse(" from lambda! Use the query string parameter 'name' to specify your name")
+		)));
+	}
+
+	private APIGatewayV2HTTPResponse notFoundResponse(APIGatewayV2HTTPEvent requestEvent) {
+		return buildResponse(SC_NOT_FOUND, HelloWorld.Body.error("The resource with method %s and path %s is not found".formatted(
+				getMethod(requestEvent),
+				getPath(requestEvent)
+		)));
+	}
+
+	private APIGatewayV2HTTPResponse buildResponse(int statusCode, Object body) {
+		return APIGatewayV2HTTPResponse.builder()
+				.withStatusCode(statusCode)
+				.withHeaders(responseHeaders)
+				.withBody(gson.toJson(body))
+				.build();
+	}
+
+	private String getMethod(APIGatewayV2HTTPEvent requestEvent) {
+		return requestEvent.getRequestContext().getHttp().getMethod();
+	}
+
+	private String getPath(APIGatewayV2HTTPEvent requestEvent) {
+		return requestEvent.getRequestContext().getHttp().getPath();
+	}
+
+	private String getUserName(Map<String, String> queryStringParameters) {
+		return queryStringParameters.get("name");
+	}
+
+
+	private record RouteKey(String method, String path) {
+	}
+
+	private record Body(String message, String error) {
+		static Object ok(String message) {
+			return new HelloWorld.Body(message, null);
+		}
+
+		static Object error(String error) {
+			return new HelloWorld.Body(null, error);
+		}
+	}
+
+}
