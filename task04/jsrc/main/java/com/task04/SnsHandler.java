51b17d04efcb35430ac7a743595ee9e4ee5ad67a
armando-guerrero <armando.guerrero@externos.digitalfemsa.com>
2024-09-05 10:23:00 -0600

    task04- sqs and sns

---
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -0,0 +1,49 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+
+import java.util.Iterator;
+import java.util.List;
+
+@LambdaHandler(
+        lambdaName = "sns_handler",
+        roleName = "sns_handler-role",
+        isPublishVersion = false,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SnsEventSource(
+        targetTopic = "lambda_topic"
+)
+public class SnsHandler implements RequestHandler<SNSEvent, Boolean> {
+    LambdaLogger logger;
+
+    @Override
+    public Boolean handleRequest(SNSEvent event, Context context) {
+        logger = context.getLogger();
+        List<SNSEvent.SNSRecord> records = event.getRecords();
+        if (!records.isEmpty()) {
+            Iterator<SNSEvent.SNSRecord> recordsIter = records.iterator();
+            while (recordsIter.hasNext()) {
+                processRecord(recordsIter.next());
+            }
+        }
+        return Boolean.TRUE;
+    }
+
+    public void processRecord(SNSEvent.SNSRecord record) {
+        try {
+            String message = record.getSNS().getMessage();
+            logger.log("message: " + message);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -2,33 +2,44 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @LambdaHandler(
-    lambdaName = "sqs_handler",
-	roleName = "sqs_handler-role",
-	isPublishVersion = false,
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED,
-	eventSources = {
-		@SqsEventSource(
-			eventSourceArn = "arn:aws:sqs:us-east-1:123456789012:async_queue",
-			batchSize = 10
-		)
-	}
+		lambdaName = "sqs_handler",
+		roleName = "sqs_handler-role",
+		isPublishVersion = false,
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class SqsHandler implements RequestHandler<Object, Map<String, Object>> {
+@SqsTriggerEventSource(
+		targetQueue = "async_queue",
+		batchSize = 1)
+@DependsOn(
+		name = "async_queue",
+		resourceType = ResourceType.SQS_QUEUE
+)
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		for (SQSEvent.SQSMessage message : event.getRecords()) {
-			System.out.println("Received message: " + message.getBody());
+public class SqsHandler implements RequestHandler<SQSEvent, Void> {
+	@Override
+	public Void handleRequest(SQSEvent sqsEvent, Context context) {
+		for (SQSEvent.SQSMessage msg : sqsEvent.getRecords()) {
+			processMessage(msg, context);
 		}
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+		context.getLogger().log("done");
+		return null;
+	}
+
+	private void processMessage(SQSEvent.SQSMessage msg, Context context) {
+		try {
+			context.getLogger().log("Processed message " + msg.getBody());
+		} catch (Exception e) {
+			context.getLogger().log("An error occurred");
+			throw e;
+		}
+
 	}
 }
