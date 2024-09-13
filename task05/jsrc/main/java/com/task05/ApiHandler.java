ce80f17d1a6c47acee18d6aa785d9f52011e1e8f
armando-guerrero <armando.guerrero@externos.digitalfemsa.com>
2024-09-12 22:27:34 -0600

    task05- 4 Dynamo and lambda

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -1,18 +1,14 @@
 package com.task05;
 
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
@@ -29,15 +25,14 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.function.Function;
 
 @LambdaHandler(
-    lambdaName = "api_handler",
-	roleName = "api_handler-role",
-	isPublishVersion = false,
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED,
-	runtime = DeploymentRuntime.JAVA17,
-	architecture = Architecture.ARM64
+		lambdaName = "api_handler",
+		roleName = "api_handler-role",
+		isPublishVersion = false,
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED,
+		runtime = DeploymentRuntime.JAVA17,
+		architecture = Architecture.ARM64
 )
 @LambdaUrlConfig(
 		authType = AuthType.NONE,
@@ -46,38 +41,28 @@ import java.util.function.Function;
 @EnvironmentVariables(value = {
 		@EnvironmentVariable(key = "target_table", value = "Events")}
 )
-public class ApiHandler  implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
 
 	private static final int SC_CREATED = 201;
-	private static final int SC_NOT_FOUND = 404;
 	private final ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
 	private final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
 	private final DynamoDB dynamoDB = new DynamoDB(client);
 	private final Table table = dynamoDB.getTable(System.getenv("target_table"));
-	private final Map<ApiHandler.RouteKey, Function<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse>> routeHandlers = Map.of(
-			new ApiHandler.RouteKey("POST", "/events"), this::handleGetEvents
-	);
 
 	@Override
-	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
-		ApiHandler.RouteKey routeKey = new ApiHandler.RouteKey(getMethod(requestEvent), getPath(requestEvent));
-		return routeHandlers.getOrDefault(routeKey, this::notFoundResponse).apply(requestEvent);
-	}
-
-	private APIGatewayV2HTTPResponse handleGetEvents(APIGatewayV2HTTPEvent requestEvent) {
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		System.out.println("Entra bien...");
 		try {
-			Map<String, Object> requestBody = objectMapper.readValue(requestEvent.getBody(), Map.class);
-			System.out.println("Request body: ........");
-			System.out.println("Request body: " + requestBody.get("content").toString());
+			Map<String, Object> requestBody = objectMapper.convertValue(request, new TypeReference<Map<String, Object>>() {});
+			requestBody.forEach((key, value) -> System.out.println("[Key] : " + key + " [Value] : " + value));
 			String id = UUID.randomUUID().toString();
 			String createdAt = Instant.now().toString();
-			System.out.println("Request body: " + requestBody.get("content").toString());
+
 			Item item = new Item()
-					.withPrimaryKey("id", id.toString())
+					.withPrimaryKey("id", id)
 					.withString("principalId", requestBody.get("principalId").toString())
 					.withString("createdAt", createdAt)
-					.withString("body", requestBody.get("content").toString());
-			System.out.println("Ready to write un table ....");
+					.withMap("body", (Map<String, String>) requestBody.get("content"));
 
 			table.putItem(item);
 
@@ -85,57 +70,13 @@ public class ApiHandler  implements RequestHandler<APIGatewayV2HTTPEvent, APIGat
 			responseBody.put("statusCode", SC_CREATED);
 			responseBody.put("event", item.asMap());
 
-			return APIGatewayV2HTTPResponse.builder()
-					.withStatusCode(SC_CREATED)
-					.withBody(objectMapper.writeValueAsString(responseBody))
-					.build();
+			return responseBody;
 		} catch (Exception e) {
-			//context.getLogger().log("Error: " + e.getMessage());
-			return APIGatewayV2HTTPResponse.builder()
-					.withStatusCode(500)
-					.withBody("{\"message\": \"Internal server error\"}")
-					.build();
+			context.getLogger().log("Error: " + e.getMessage());
+			Map<String, Object> errorResponse = new HashMap<>();
+			errorResponse.put("statusCode", 500);
+			errorResponse.put("message", "Internal server error");
+			return errorResponse;
 		}
-	}
-
-	private APIGatewayV2HTTPResponse notFoundResponse(APIGatewayV2HTTPEvent requestEvent) {
-		return buildResponse(SC_NOT_FOUND, ApiHandler.Body.statusCode("Bad request syntax or unsupported method. Request path: /cmtr-12e2dc5a. HTTP method: POST".formatted(
-				getMethod(requestEvent),
-				getPath(requestEvent)
-		)));
-	}
-
-	private String getMethod(APIGatewayV2HTTPEvent requestEvent) {
-		return requestEvent.getRequestContext().getHttp().getMethod();
-	}
-
-	private String getPath(APIGatewayV2HTTPEvent requestEvent) {
-		return requestEvent.getRequestContext().getHttp().getPath();
-	}
-
-	private record RouteKey(String method, String path) {
-	}
-
-	private APIGatewayV2HTTPResponse buildResponse(int statusCode, Object body) {
-		try {
-			return APIGatewayV2HTTPResponse.builder()
-					.withStatusCode(statusCode)
-					.withBody(objectMapper.writeValueAsString(body))
-					.build();
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private record Body(String message, int statusCode) {
-		static Object statusCode(String statusCode) {
-			return new ApiHandler.Body("Bad request syntax or unsupported method. Request path: /cmtr-12e2dc5a. HTTP method: GET", 400);
-		}
-
-		static Object ok(String message) {
-			return new ApiHandler.Body(message,200);
-		}
-
-
 	}
 }
